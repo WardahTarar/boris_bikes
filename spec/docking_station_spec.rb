9dae46219bc8d6a5501f8e13b0f7d13bcad265c8
Student <student@Makerss-MacBook-Pro.local>
2019-03-05 17:34:29 +0000

    Makes docking stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,17 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do
   it "will respond to release_bike" do
     instance = DockingStation.new
     expect(instance).to respond_to(:release_bike)
+  end
+
+  it "will get bike" do
+    # when release_bike is called...
+    # it creates a new Bike instance
+
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to be_an_instance_of(Bike)
   end
 end
