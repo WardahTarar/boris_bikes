6613a9ac3d20c1b3a442b052f52dfc1482c7b5e2
Student <student@Makerss-MacBook-Pro.local>
2019-03-05 16:38:31 +0000

    Responds to release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
-require "docking_station.rb"
+require "docking_station"
 
-describe "Docking Station" do
+describe DockingStation do
+  it "will respond to release_bike" do
+    instance = DockingStation.new
+    expect(instance).to respond_to(:release_bike)
+  end
 end
