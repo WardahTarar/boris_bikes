635d20b31dbd1eac7126b50e74ecf2400abde5d8
Student <student@Makerss-MacBook-Pro.local>
2019-03-06 16:06:11 +0000

    Created a bike list attribute as a new Array, then passed in bikes using dock method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,17 @@
-require "bike"
+require_relative "bike"
 
 class DockingStation
+  attr_reader :bike_list
+
   def initialize
+    @bike_list = Array.new
   end
 
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @bike_list << bike 
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,35 @@
 require "docking_station"
-require "bike"
 
 describe DockingStation do
   it "will respond to release_bike" do
-    instance = DockingStation.new
-    expect(instance).to respond_to(:release_bike)
+    victoria_station = DockingStation.new
+    expect(victoria_station).to respond_to(:release_bike)
   end
 
   it "will get bike" do
     # when release_bike is called...
     # it creates a new Bike instance
 
-    docking_station = DockingStation.new
-    expect(docking_station.release_bike).to be_an_instance_of(Bike)
+    victoria_station = DockingStation.new
+    expect(victoria_station.release_bike).to be_an_instance_of(Bike)
+  end
+
+  it 'dock bike at docking station' do
+    victoria_station = DockingStation.new
+    expect(victoria_station).to respond_to(:dock)
+  end
+
+  it 'add 1 bike to docked_bikes' do
+    victoria_station = DockingStation.new
+    bike1 = Bike.new
+    #victoria_station.dock(bike)
+    expect(victoria_station.dock(bike1))
+  end
+
+  it 'add 1 bike to docked_bikes' do
+    victoria_station = DockingStation.new
+    bike = Bike.new
+    victoria_station.dock(bike)
+    expect(victoria_station.bike_list).to include(bike)
   end
 end
